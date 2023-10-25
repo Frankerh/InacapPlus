20d09e795dbf7e3b87065fa2a35e739d169d2a3e
Frankerh <fcobustamante313@gmail.com>
2023-10-25 03:12:58 -0300

    25-10

--- app/src/main/java/com/example/myapplication/Perfil.java
+++ app/src/main/java/com/example/myapplication/Perfil.java
@@ -1,17 +1,15 @@
 package com.example.myapplication;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import com.bumptech.glide.Glide;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.*;
+import com.bumptech.glide.Glide;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
--- app/src/main/java/com/example/myapplication/Registrar.java
+++ app/src/main/java/com/example/myapplication/Registrar.java
@@ -1,7 +1,108 @@
 package com.example.myapplication;
+
+import static androidx.core.content.ContextCompat.startActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Registrar extends AppCompatActivity {
 
+    private EditText emailText;
+    private EditText Nametext;
+    private EditText passwordText;
+    private Button loginButton;
+
+    // Variables de datos a registrar
+    private String email = "";
+    private String name = "";
+    private String password = "";
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registrar_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        emailText = findViewById(R.id.emailText);
+        Nametext = findViewById(R.id.NameText);
+        passwordText = findViewById(R.id.passwordText);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = emailText.getText().toString();
+                name = Nametext.getText().toString();
+                password = passwordText.getText().toString();
+
+                if (!name.isEmpty() && !email.isEmpty() && !password.isEmpty()) {
+                    if (password.length() >= 6) {
+                        registerUser();
+                    } else {
+                        Toast.makeText(Registrar.this, "La contraseña debe tener más de 6 caracteres", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(Registrar.this, "Debe completar los campos requeridos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void registerUser() {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Map<String, Object> user = new HashMap<>();
+                    user.put("email", email);
+                    user.put("name", name);
+                    user.put("password", password);
+
+                    String uid = mAuth.getCurrentUser().getUid();
+
+                    db.collection("Users").document(uid).set(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task2) {
+                            if (task2.isSuccessful()) {
+                                startActivity(new Intent(Registrar.this, MainActivity.class));
+                                finish();
+                            } else {
+                                Toast.makeText(Registrar.this, "Los datos no se registraron correctamente", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(Registrar.this, "No se pudo registrar el usuario", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
 }
+
 
